a04915867aa44150595e4eaaee75e90bbaf9e297
Daniel Aristizabal <daniel.aristizabal@sofka.com.co>
2023-03-21 15:51:35 -0500

    Se crea funcion para obtener pago segun el estado de la persona

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -24,15 +24,23 @@
     }
 
 
-    // Crea un nuevo actor
     function createActor( fullName: string, birthdate: Date ): boolean {
         
-
         if ( fullName === 'fernando' ) return false;
 
         console.log('Crear actor');
         return true;        
 
+    }
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+
+        if ( isDead ) return 1500;
+        
+        if ( isSeparated ) return 2500;
+
+        return isRetired ? 3000 : 4000; 
+        
     }
 
 
